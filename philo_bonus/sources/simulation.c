be327f69b3acae90b6d0feeb5fa32cd74985d6fa
Narek Tamazyan <natamazy@gmail.com>
2024-10-07 21:39:00 +0400

    fixed

--- philo_bonus/sources/simulation.c
+++ philo_bonus/sources/simulation.c
@@ -6,7 +6,7 @@
 /*   By: natamazy <natamazy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 15:32:34 by natamazy          #+#    #+#             */
-/*   Updated: 2024/04/18 20:58:14 by natamazy         ###   ########.fr       */
+/*   Updated: 2024/10/07 21:37:10 by natamazy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,13 +43,13 @@ void	*start_sim(t_philo *philo)
 		print_message(philo, "has taken a fork\n");
 		print_message(philo, "is eating\n");
 		my_sleep(philo->all_info->eat_time);
-		sem_post(philo->all_info->forks);
-		sem_post(philo->all_info->forks);
+		lock_last_eat(philo);
 		sem_wait(philo->all_info->each_eat_sem);
 		philo->eat_counter++;
 		sem_post(philo->all_info->each_eat_sem);
+		sem_post(philo->all_info->forks);
+		sem_post(philo->all_info->forks);
 		print_message(philo, "is sleeping\n");
-		lock_last_eat(philo);
 		my_sleep(philo->all_info->sleep_time);
 		print_message(philo, "is thinking\n");
 	}
